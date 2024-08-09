3d87a952c933e4c8e18ab882e5d294acaa8f1706
kwrona05 <kwrona05@gmail.com>
2024-08-09 11:26:36 +0200

    base

---
+++ Shop page/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+function App() {
+  return (
+    <div>
+      <div>
+      <p>Electronic</p>
+      <p>Food and drinks</p>
+      <p>Furniture</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
