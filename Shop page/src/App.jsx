709c19dc439e732925f413e77a5bddc49a0fd646
kwrona05 <kwrona05@gmail.com>
2024-08-09 12:01:05 +0200

    First idea

--- Shop page/src/App.jsx
+++ Shop page/src/App.jsx
@@ -1,4 +1,17 @@
 import React from "react"
+import './App.css'
+
+function Tile(props) {
+  return (
+    <li className="tile">
+  <img src={props.img} alt={props.title}></img>
+  <h3>{props.title}</h3>
+  <p>{props.date}</p>
+  <p>{props.sale}</p>
+  <p>{props.details}</p>
+  </li>
+  )
+} 
 function App() {
   return (
     <div>
@@ -7,6 +20,17 @@ function App() {
       <p>Food and drinks</p>
       <p>Furniture</p>
       </div>
+      <Tile 
+      title="Snickers"
+      date="To 10.08.2024"
+      sale="40%"
+      details="You have to buy two snacks"
+      />
+      <Tile 
+      title="Iphone"
+      date="-"
+      sale="20%"
+      details="If you are a student you have 20% off on Ispot" />
     </div>
   )
 }
