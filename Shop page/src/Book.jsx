ecc84d2fdf0b996a2358a3a2c2967583fcd00a5b
kwrona05 <kwrona05@gmail.com>
2024-08-11 10:46:58 +0200

    Update Book.jsx

--- Shop page/src/Book.jsx
+++ Shop page/src/Book.jsx
@@ -1,8 +1,21 @@
 
+const bookTile = (props) => {
+    return(
+        <div>
+            <img src={props.image} alt={props.title}></img>
+            <h2>{props.title}</h2>
+            <a>Read more</a>
+        </div>
+    )
+}
+
 function Book() {
     return(
         <div>
-
+            <bookTile 
+            image = ""
+            title="StarWars"
+            />
         </div>
     )
 }
